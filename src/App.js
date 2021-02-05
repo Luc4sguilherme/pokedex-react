dcea71342948f3ba043cbbfff22912ef5572d979
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2021-02-05 14:28:22 -0300

    change loader element

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const amountOfPoKemons = 1118
   const limit = 50
 
+  let [gridColumnCount, setGridColumnCount] = useState(1) 
   let [pokemons, setPokemons] = useState([])
   let [offset, setOffset] = useState(0)
   let [hasMore, setHasMore] = useState(true)
@@ -36,6 +37,13 @@ function App() {
     fetchMoreData()
   }, [])
 
+  useEffect(() => {
+    const grid = document.querySelector('.pokedex')
+    const gridComputedStyle = window.getComputedStyle(grid);
+    const gridColumn = gridComputedStyle.getPropertyValue("grid-template-columns").split(" ")
+    setGridColumnCount(gridColumn.length);
+  }, []);
+
   return (
     <div className="container">
       <h1>Pokédex</h1>
@@ -44,7 +52,15 @@ function App() {
         dataLength={pokemons.length}
         next={fetchMoreData}  
         hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
+        loader={
+        <div style={{
+          gridColumn: `span ${gridColumnCount}`,
+          justifySelf: 'center',
+          padding: '15px'
+        }}>
+          <div className='loader'></div>
+        </div>
+        }
       >
         {pokemons.map((pokemon) => (
           <Card
